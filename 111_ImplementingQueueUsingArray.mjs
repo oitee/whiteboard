be63e23a6866d68505cf35df1805b6f53c6052c2
oitee <oitee.codes@gmail.com>
2021-08-09 17:30:39 +0530

    Add a program implementing queue datastructure using array

---
+++ 111_ImplementingQueueUsingArray.mjs
@@ -0,0 +1,42 @@
+export function Queue(){
+    this.arr = [];
+    this.headIndex = 0;
+    this.tailIndex = 0;
+    this.isEmpty = function(){
+        if(this.arr.length == 0){
+            return true
+        };
+        return this.headIndex > this.tailIndex;
+    };
+    this.enqueue = function(data){
+        if(this.tailIndex == 0){
+            this.arr[this.tailIndex] = data;
+        }
+        this.arr[++this.tailIndex] = data;
+        return data;
+    };
+    this.dequeue = function(){
+        if(this.isEmpty()){
+            return "Queue is empty";
+        }
+        let dequeued = this.arr[this.headIndex++];
+        return dequeued;
+    };
+    this.peek = function(){
+        if(this.isEmpty()){
+            return "Queue is empty";
+        }
+        return this.arr[this.tailIndex];
+    }
+}
+
+let q = new Queue();
+console.log(q.isEmpty() == true);
+console.log(q.peek() == "Queue is empty");
+console.log(q.dequeue() == "Queue is empty");
+console.log(q.enqueue(1) == 1);
+q.enqueue(2);
+q.enqueue(3);
+console.log(q.dequeue() == 1);
+console.log(q.peek() == 3);
+console.log(q.isEmpty() == false);
