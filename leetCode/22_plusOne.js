f11e3665e856970411c943689e0f436d942e1a8b
oitee <oitee.codes@gmail.com>
2021-08-09 21:12:13 +0530

    Add a program to solve a LeetCode problem to add one to an array containing digits of a number

---
+++ leetCode/22_plusOne.js
@@ -0,0 +1,23 @@
+//Given an array of integers representing a number, where each element represents one digit of that number, add one to the number
+//And return the array
+// https://leetcode.com/problems/plus-one/
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] != 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  if (digits[0] == 0) {
+    digits.unshift(1);
+    return digits;
+  }
+};
+
+console.log(plusOne([1,2,3]));
+console.log(plusOne([4,3,2,1]));
+console.log(plusOne([1,2,3,9]));
+console.log(plusOne([9,9,9]));
+console.log(plusOne([0]));
