e2df72f9da8fc166d4a486181951313fdb0ea592
oitee <oitee.codes@gmail.com>
2021-08-12 09:39:31 +0530

    Fix errors in the program deleting a node in a binary search tree

--- 118_deleteBST.js
+++ 118_deleteBST.js
@@ -14,16 +14,16 @@ function deleteBST(root, node) {
       direction = "right";
     }
   }
-//when the target node is childless
-//its parent should point to null instead of the target node
+  //when the target node is childless
+  //its parent should point to null instead of the target node
   if (node.right == null && node.left == null) {
-    if(parentNode == undefined){
+    if (parentNode == undefined) {
       return;
     }
     parentNode[direction] = null;
   }
-//when the target node has one child
-//target node's parent should point to the target node's only child
+  //when the target node has one child
+  //target node's parent should point to the target node's only child
   if (node.right == null) {
     if (parentNode == undefined) {
       root = root.left;
@@ -40,10 +40,10 @@ function deleteBST(root, node) {
     parentNode[direction] = node.right;
     return root;
   }
- //when the target node has both children
- //we need to find its immediate successor
- //and replace its value with that of the target node
- //the immediate successor will be the left-most node in the target node's right sub-tree
+  //when the target node has both children
+  //we need to find its immediate successor
+  //and replace its value with that of the target node
+  //the immediate successor will be the left-most node in the target node's right sub-tree
 
   let successorNode = findSmallestInRightSubTree(node);
   if (successorNode == node.right) {
@@ -55,13 +55,13 @@ function deleteBST(root, node) {
   return root;
 }
 
-function findSmallestInRightSubTree(node){
-  if(node.right.left == null){
+function findSmallestInRightSubTree(node) {
+  if (node.right.left == null) {
     return node.right;
   }
   let parent = node.right;
   leftchild = parent.left;
-  while(leftchild.left != null){
+  while (leftchild.left != null) {
     parent = leftchild;
     leftchild = leftchild.left;
   }
@@ -69,32 +69,33 @@ function findSmallestInRightSubTree(node){
   parent.left = null;
   return returnValue;
 }
-  
 
-function findParent(root, targetNode){
-  if(root == targetNode){
+function findParent(root, targetNode) {
+  if (root == targetNode) {
     return;
   }
-  /*console.log(root.data);
-  console.log(root == targetNode);
-  console.log("target");
-  console.log(targetNode);
-  console.log();*/
-  if(root.left.data == targetNode.data || root.right.data == targetNode.data){
-    return root;
+  if (root.left != null) {
+    if (root.left.data == targetNode.data) {
+      return root;
+    }
   }
-  if(targetNode.data < root.data){
+  if (root.right != null) {
+    if (root.right.data == targetNode.data) {
+      return root;
+    }
+  }
+  if (targetNode.data < root.data) {
     return findParent(root.left, targetNode);
-  }
-  else{
+  } else {
     return findParent(root.right, targetNode);
   }
-
 }
 
 //testing
+function test(){
 let root1 = arrayToBst.arrayToBst([99, 100, 121, 13, -1, 881, 12]);
 console.log(levelorder.levelOrderTraversal(root1));
-//console.log(root1.right.right.data);
-deleteBST(root1, root1.right.right.data);
+deleteBST(root1, root1.right.right);
 console.log(levelorder.levelOrderTraversal(root1));
+}
+//test()
